092d0c6822301b4e3c49753c45918083c21e85fa
David Scott <davidnscott.ja@gmail.com>
2020-11-10 18:29:05 -0500

    Completed webpage

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-window.onload=function start(){
-	let search = document.querySelector(".btn");
-	search.addEventListener('click', function(){
+window.onload=function(){
+	let search = document.getElementById("btn");
+	search.addEventListener('click', function(e){
+		e.preventDefault();
+		var s = document.getElementById("search").value;
 		var req = new XMLHttpRequest();
 		req.onreadystatechange = function() {
 			if (req.readyState == XMLHttpRequest.DONE) {
-				alert(req.responseText);
+				if (req.status == 200){	
+					var r = req.responseText;
+					var result = document.getElementById('result');
+					result.innerHTML = r;
+				}else{
+					alert("Error!");
+				}
 			}
 		}
-		req.open('GET', 'http://localhost:8888/info2180-lab4/superheroes.php', true);
-		req.send(null);
+		console.log(s);
+		req.open('GET', 'http://localhost:8888/info2180-lab4/superheroes.php?query='+s, true);
+		req.send();
 	});
 }
 	
